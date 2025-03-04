f6f46c90debc72792c140028fa559efdf558f393
Chirayu <masterchirayujain@gmail.com>
2025-03-04 23:18:53 +0530

    delete

--- app/Repositories/ServiceRepository.php
+++ app/Repositories/ServiceRepository.php
@@ -25,4 +25,23 @@ class ServiceRepository
         $data['points'] = json_encode($data['points'] ?? []);
         return Service::create($data);
     }
+    public function deleteByName($name): bool
+{
+    try {
+        // Delete all services with the given name
+        $deleted = Service::where('service_name', $name)->delete();
+
+        if ($deleted > 0) {
+            \Log::info("Deleted services with name: " . $name);
+            return true;
+        } else {
+            \Log::warning("No service found with name: " . $name);
+            return false;
+        }
+    } catch (\Exception $e) {
+        \Log::error("Error deleting service: " . $e->getMessage());
+        return false;
+    }
+}
+
 }
